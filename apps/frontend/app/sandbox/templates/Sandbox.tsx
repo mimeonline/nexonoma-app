ac7fc008d537e1ce3b3a4e8b46ce0afef6b3eead
Michael Meierhoff <michael.meierhoff@gmail.com>
2025-12-06 21:10:29 +0100

    chore(frontend): update sandbox setup and gitignore

--- apps/frontend/app/sandbox/templates/Sandbox.tsx
+++ apps/frontend/app/sandbox/templates/Sandbox.tsx
@@ -1,6 +1,8 @@
+import { CityView } from "./CityView";
+
 export default function Sandbox() {
   if (process.env.NODE_ENV !== "development") {
     return null; // Seite zeigt nichts in Prod
   }
-  return <div>Sandbox</div>;
+  return <CityView></CityView>;
 }
